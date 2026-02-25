9a7bc6b14fe7b4cfae04fa89743e3c2e75be3189
Markin Pavel <markinpashka@gmail.com>
2026-02-25 08:23:09 +0300

    Comments lesson 2_10

--- src/Main.kt
+++ src/Main.kt
@@ -2,13 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
+    /** Именование классов должнг быть в CamelCase, хотя тут класс вообще не нужен */
     class test(val name: String) {
 
         @Deprecated(
-            message = "sdfsdf"
+            message = "sdfsdf" /** нельзя так писать, потрать 10 секунд на нормальное сообщение  */
                     replaceWith = ReplaceWith("printEmployeeName()")
 
         )
+        /** Ты проверил, хоть раз что-то распечаталось? */
         fun printName(name: String): String {
             return name
             println(name)

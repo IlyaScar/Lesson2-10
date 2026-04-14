ddb18e80bed8eb95a0067b783ae3977348fabc7f
Ilya Smolin <ilyasmolin9@gmail.com>
2026-04-14 21:30:38 +0300

    Fix1

--- src/Main.kt
+++ src/Main.kt
@@ -1,32 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
     /** Именование классов должнг быть в CamelCase, хотя тут класс вообще не нужен */
-    class test(val name: String) {
-
         @Deprecated(
-            message = "sdfsdf" /** нельзя так писать, потрать 10 секунд на нормальное сообщение  */
+            "Use newFunction", /** нельзя так писать, потрать 10 секунд на нормальное сообщение  */
                     replaceWith = ReplaceWith("printEmployeeName()")
-
         )
         /** Ты проверил, хоть раз что-то распечаталось? */
         fun printName(name: String): String {
             return name
-            println(name)
         }
-
         fun printEmployeeName(name: String): String {
             return name
-            println(name)
         }
-
-    }
-
-    val test1 = test("Test")
-    test1.printName("Oleg")
-
-
+    println(printName("Natasha"))
+    println(printEmployeeName("Olya"))
 }
 
 
